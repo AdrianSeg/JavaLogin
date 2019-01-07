053a6727ae0a1a487655ef2fabb5ccad4d0674d7
alpineberlinette <alpineberlinette@PC-Hundemer.fritz.box>
2019-01-07 18:18:20 +0100

    xf

--- Javalogin/src/GUILoginScreen.java
+++ Javalogin/src/GUILoginScreen.java
@@ -11,13 +11,16 @@ import javax.swing.JToolBar;
 import javax.swing.BoxLayout;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUILoginScreen extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField textFieldUser;
 	private JPasswordField passwordField;
 
 	/**
@@ -60,13 +63,29 @@ public class GUILoginScreen extends JFrame {
 		panel.add(lblPassword);
 		
 		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String user=textFieldUser.getText();
+				String password=passwordField.getText();
+				
+				if(user.equals("name") && password.equals("password"))
+				{
+					JOptionPane.showMessageDialog(frame, "you are eingelogt");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(frame, "wrong Benutzernahme");
+				}
+			}
+		});
 		btnNewButton.setBounds(125, 150, 89, 23);
 		panel.add(btnNewButton);
 		
-		textField = new JTextField();
-		textField.setBounds(200, 52, 75, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		textFieldUser = new JTextField();
+		textFieldUser.setBounds(200, 52, 75, 20);
+		panel.add(textFieldUser);
+		textFieldUser.setColumns(10);
 		
 		passwordField = new JPasswordField();
 		passwordField.setBounds(200, 100, 75, 20);
